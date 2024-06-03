c3b69c33d9b7d3a73a3f857b0c877962c1016c99
JJyen <gdb1632@naver.com>
2024-06-04 03:16:43 +0900

    FinalPlanList.js 추가

---
+++ src/pages/FinalPlanList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class FinalPlanList extends React.Component{
+    render(){
+        return(
+            <>계획서 최종 리스트</>
+        )
+    }
+}
+
+export default FinalPlanList;
