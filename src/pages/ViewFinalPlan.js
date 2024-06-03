cf0f02bfd6218fc5a5bdc59ff86bb0ee279abc8b
JJyen <gdb1632@naver.com>
2024-06-04 03:14:39 +0900

    ViewFinalPlan.js 추가

---
+++ src/pages/ViewFinalPlan.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class ViewFinalPlan extends React.Component{
+    render(){
+        return(
+            <>계획서 최종 보기</>
+        )
+    }
+}
+
+export default ViewFinalPlan;
