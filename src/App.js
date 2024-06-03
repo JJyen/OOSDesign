c5a18d8bd69398debe1c3c0bf4bf6b6b112d4020
JJyen <gdb1632@naver.com>
2024-06-04 03:35:20 +0900

    라우팅 초기 설정

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import Signin from "./pages/Signin";
 import Layout from "./pages/Layout";
+import Home from "./pages/Home";
+import FinalPlanList from "./pages/FinalPlanList";
+import ViewFinalPlan from "./pages/ViewFinalPlan";
+import MakePlan from "./pages/MakePlan";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout/>}></Route>
-        <Route path="/" element={<Signin/>}/>
+        <Route element={<Layout/>}>
+          <Route Home path="/" element={<Home/>}/>
+          <Route FinalPlanList path="/finalplanlist" element={<FinalPlanList/>}/>
+          <Route ViewFinalPlan path="/viewfinalplan" element={<ViewFinalPlan/>}/>
+          <Route MakePlan path="/makeplan" element={<MakePlan/>}/>
+        </Route>
+        <Route path="/signin" element={<Signin/>}/>
+        <Route path="/signup" element={<Signup/>}/>
       </Routes>
     </BrowserRouter>
   );
