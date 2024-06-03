08cb9653e658c87c9d727be8fa364c3dca307ca5
JJyen <gdb1632@naver.com>
2024-06-04 04:57:25 +0900

    라우트  추가

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import FinalPlanList from "./pages/FinalPlanList";
 import ViewFinalPlan from "./pages/ViewFinalPlan";
 import MakePlan from "./pages/MakePlan";
 import Signup from "./pages/Signup";
+import Account from "./pages/Account";
+import ViewWeather from "./pages/ViewWeather";
+import ViewExchageRate from "./pages/ViewExchageRate";
+import ViewMap from "./pages/ViewMap";
 
 function App() {
   return (
@@ -16,6 +20,10 @@ function App() {
           <Route FinalPlanList path="/finalplanlist" element={<FinalPlanList/>}/>
           <Route ViewFinalPlan path="/viewfinalplan" element={<ViewFinalPlan/>}/>
           <Route MakePlan path="/makeplan" element={<MakePlan/>}/>
+          <Route MakePlan path="/account" element={<Account/>}/>
+          <Route MakePlan path="/viewweather" element={<ViewWeather/>}/>
+          <Route MakePlan path="/viewexchagerate" element={<ViewExchageRate/>}/>
+          <Route MakePlan path="/viewmap" element={<ViewMap/>}/>
         </Route>
         <Route path="/signin" element={<Signin/>}/>
         <Route path="/signup" element={<Signup/>}/>
