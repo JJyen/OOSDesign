3be664d0a7474e564fb508632e50c6c51a7e850f
JJyen <gdb1632@naver.com>
2024-06-04 19:17:37 +0900

    계획 컴포넌트 추가

--- src/pages/FinalPlanList.js
+++ src/pages/FinalPlanList.js
@@ -1,11 +1,34 @@
 import React from "react";
+import PlanListItem from "../components/PlanListItem";
+import styled from "styled-components";
 
 class FinalPlanList extends React.Component{
     render(){
         return(
-            <>계획서 최종 리스트</>
+            <FinalPlanListContainer>
+                <PlanListItem/>
+                <PlanListItem/>
+                <PlanListItem/>
+                <PlanListItem/>
+            </FinalPlanListContainer>
         )
     }
 }
 
 export default FinalPlanList;
+
+const FinalPlanListContainer = styled.div`
+    display: flex;
+    align-items: center;
+    flex-direction: column; 
+    background-color: white;
+    margin-top: 30px;
+    padding-bottom: 90px;
+
+    div{
+        height: 55px;
+        margin-bottom: 20px;
+        border-radius: 20px;
+        border: 3px solid #41315C;
+    }
+`;
