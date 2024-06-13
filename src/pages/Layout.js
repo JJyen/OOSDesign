72e2fe8d3b4dd5977e619ef39283b307bd4f2836
JJyen <gdb1632@naver.com>
2024-06-14 06:09:14 +0900

    로그인 상태 추가

--- src/pages/Layout.js
+++ src/pages/Layout.js
@@ -3,18 +3,32 @@ import { Outlet } from "react-router-dom";
 import TapBar from "../components/TapBar";
 import styled from "styled-components";
 import { Link } from "react-router-dom";
+import { MAIN } from "../styles/Colors";
 
 class Layout extends React.Component{
+    constructor(props) {
+        super(props);
+        const user = JSON.parse(localStorage.getItem("user"));
+
+        this.state = {
+            login: user[0].login,
+        };
+    }
+
     render(){
+        const {login} = this.state;
+
         return(
             <LayoutContainer >
                 <Link to={"/"}><h2>트리뷰</h2></Link>
-                <main>
-                    <Outlet/>
-                </main>
-                <nav>
-                    <TapBar/>
-                </nav>
+                { login ? <main><Outlet/></main>
+                : 
+                <NotLoggedInBox>
+                    <h3>로그아웃 상태입니다.</h3>
+                    <Link to="/signin"><button>로그인 하기</button></Link>
+                </NotLoggedInBox>
+                }
+                <nav><TapBar/></nav>
             </LayoutContainer>
         )
     }
@@ -35,7 +49,26 @@ const LayoutContainer = styled.div`
 
     h2{
         text-align: center;
-        padding-top: 10px;
+        padding-top: 20px;
         font-family: 'NPSfontBold';
+    }
+`;
+
+const NotLoggedInBox = styled.div`
+    text-align: center;
+    margin-top: 40%;
+
+    button{
+        margin-top: 15px;
+        width: 180px;
+        height: 40px;
+        border: none;
+        background-color: ${MAIN};
+        border-radius: 10px;
+
+        &:hover{
+            cursor: pointer;
+            background-color: #B3A3BA;
+        }
     }
 `;
